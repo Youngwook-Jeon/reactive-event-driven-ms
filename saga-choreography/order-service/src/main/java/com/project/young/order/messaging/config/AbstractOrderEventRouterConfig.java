4db8fd3b29e9a98b836f8229564072f3af2c0e01
Youngwook-Jeon <ela87@naver.com>
2023-08-19 18:20:12 +0900

    Build stream bindings for order events

---
+++ saga-choreography/order-service/src/main/java/com/project/young/order/messaging/config/AbstractOrderEventRouterConfig.java
@@ -0,0 +1,39 @@
+package com.project.young.order.messaging.config;
+
+import com.project.young.common.events.DomainEvent;
+import com.project.young.common.events.order.OrderEvent;
+import com.project.young.common.processor.EventProcessor;
+import com.project.young.common.util.MessageConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+public abstract class AbstractOrderEventRouterConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(AbstractOrderEventRouterConfig.class);
+    private static final String DESTINATION_HEADER = "spring.cloud.stream.sendto.destination";
+    private static final String ORDER_EVENTS_CHANNEL = "order-events-channel";
+
+    protected <T extends DomainEvent> Function<Flux<Message<T>>, Flux<Message<OrderEvent>>> processor(EventProcessor<T, OrderEvent> eventProcessor) {
+        return flux -> flux.map(MessageConverter::toRecord)
+                .doOnNext(r -> log.info("order service received {}", r.message()))
+                .concatMap(r -> eventProcessor.process(r.message())
+                        .doOnSuccess(e -> r.acknowledgement().acknowledge())
+                )
+                .map(this::toMessage);
+    }
+
+    protected Message<OrderEvent> toMessage(OrderEvent event) {
+        log.info("order service produced {}", event);
+        return MessageBuilder.withPayload(event)
+                .setHeader(KafkaHeaders.KEY, event.orderId().toString())
+                .setHeader(DESTINATION_HEADER, ORDER_EVENTS_CHANNEL)
+                .build();
+    }
+
+}
